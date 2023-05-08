fb2879e4bfd57ab240adb5d8c228b4fe423786c7
Mislav <mislav.crnkovic@inxelo.com>
2023-05-08 14:26:48 +0200

    ports changed

--- src/consts/ServerInfo.js
+++ src/consts/ServerInfo.js
@@ -3,7 +3,7 @@
 export function getServerPath() {
     let fullUrl = window.location.href;
     if(fullUrl.includes("localhost")) {
-        return 'http://localhost:8080'
+        return 'http://localhost:8081'
     } else {
         const subdomain = fullUrl.split('.')[0];
         return subdomain + '.inxelo.aero/icarussms';
