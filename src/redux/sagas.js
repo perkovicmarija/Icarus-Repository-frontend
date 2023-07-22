1b2db21679cc36e7eebebfd9a96ff784c1654f80
Mislav0508 <mislav.crnkovic@inxelo.com>
2023-07-22 11:52:36 +0200

    merged master

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -22,7 +22,7 @@ export default function* rootSaga(getState) {
         auditChecklistSaga(),
         auditChecklistItemSaga(),
         auditChecklistSubAreaSaga(),
-        settingSagas()
+        settingSagas(),
         auditChecklistSubAreaSaga(),
         auditorActionLocationType()
     ]);
