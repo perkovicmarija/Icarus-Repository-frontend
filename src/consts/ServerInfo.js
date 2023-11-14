43be9f6dab703f97070cac0e2b175332b251d604
croraf <vrafaeli@msn.com>
2023-11-14 21:47:57 +0100

    ServerInfo - target test server

--- src/consts/ServerInfo.js
+++ src/consts/ServerInfo.js
@@ -2,7 +2,7 @@ export function getServerPath() {
   let fullUrl = window.location.href;
   if (fullUrl.includes("localhost")) {
     //return 'http://localhost:8081';
-    return "https://repository.inxelo.aero/icarussms";
+    return "https://repository-test.inxelo.aero/icarussms";
   } else {
     const subdomain = fullUrl.split(".")[0];
     return subdomain + ".inxelo.aero/icarussms";
