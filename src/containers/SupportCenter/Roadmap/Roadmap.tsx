7860067c3213ac5df07dd801566490596bbad170
croraf <vrafaeli@msn.com>
2023-11-17 07:52:06 +0100

    Roadmap - refactor Filters and list

--- src/containers/SupportCenter/Roadmap/Roadmap.tsx
+++ src/containers/SupportCenter/Roadmap/Roadmap.tsx
@@ -1,100 +1,32 @@
-import { useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { useLayoutEffect, useMemo, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/store";
-import { usePagination } from "../../../helpers/pagination";
-import { useHistory } from "react-router-dom";
+//import { usePagination } from "../../../helpers/pagination";
+//import { useHistory } from "react-router-dom";
 import DialogFormFrame from "../../../components/core/Dialog/DialogFormFrame";
 //
-
-import Tooltip from "@mui/material/Tooltip";
-import IconButton from "@mui/material/IconButton";
-import AddComment from "@mui/icons-material/AddComment";
-import { makeStyles } from "@mui/styles";
-import { Paper } from "@mui/material";
-
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
-
-import Typography from "@mui/material/Typography";
-import BuildIcon from "@mui/icons-material/Build";
-import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
-import FlightIcon from "@mui/icons-material/Flight";
 import DialogFormRoadmap from "../../../components/support/DialogFormRoadmap";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-//import DialogFormRoadmapLogFilter from "../../../components/support/DialogFormRoadmapLogFilter";
+import DialogFormRoadmapLogFilter from "../../../components/support/DialogFormRoadmapLogFilter";
 import TableToolbar2 from "../../../components/core/Table/TableToolbar2";
-import { DialogDelete2 } from "../../../components/core/Dialog/DialogDelete2";
 import {
+  FiltersType,
   RoadmapType,
+  initFilters,
   roadmapActions,
 } from "../../../redux/support/roadmapSlice";
-
-const useStyles = makeStyles((theme: any) => ({
-  paper: {
-    padding: "6px 16px",
-  },
-  timelineSuccess: {
-    background: "green",
-  },
-  timelineInProgress: {
-    background: "#c3922e",
-  },
-  timelineNeutral: {
-    background: "#043076",
-  },
-  iconContainer: {
-    display: "flex",
-    justifyContent: "flex-end",
-    marginBottom: theme.spacing(1),
-    marginTop: theme.spacing(1),
-  },
-  deleteIcon: {
-    color: "#d32f2f",
-  },
-  marginY: {
-    marginBottom: theme.spacing(1),
-    marginTop: theme.spacing(1),
-  },
-}));
-
-const checkIcon = (value: any) => {
-  if (value === "completed") {
-    return <EmojiEventsIcon />;
-  }
-  if (value === "in-progress") {
-    return <BuildIcon />;
-  }
-  return <FlightIcon />;
-};
+import { RoadmapList } from "./RoadmapList";
 
 function Roadmap() {
-  const classes = useStyles();
-  const checkIconColor = (value: any) => {
-    if (value === "completed") {
-      return classes.timelineSuccess;
-    }
-    if (value === "in-progress") {
-      return classes.timelineInProgress;
-    }
-    return classes.timelineNeutral;
-  };
-
   const dispatch = useAppDispatch();
-  const history = useHistory();
+  //const history = useHistory();
 
   const data = useAppSelector((state) => state.Roadmap.data);
-  const totalCount = useAppSelector((state) => state.Roadmap.meta.totalCount);
+  //const totalCount = useAppSelector((state) => state.Roadmap.meta.totalCount);
   const filters = useAppSelector((state) => state.Roadmap.filters);
 
-  const [dialogWarning, setDialogWarning] = useState<RoadmapType>();
   const [dialogAddEdit, setDialogAddEdit] = useState<
     RoadmapType | {} | undefined
   >();
+  const [dialogFilters, setDialogFilters] = useState<FiltersType | undefined>();
 
   const page = 0;
   const rowsPerPage = 50;
@@ -119,82 +51,51 @@ function Roadmap() {
     );
   }, [meta]);
 
+  const handleSubmitFilters = (newFilters: FiltersType) => {
+    dispatch(roadmapActions.setFilters({ ...filters, ...newFilters }));
+    // history.push(getUsersPath(0));
+  };
+  // PAGINATION
+  /* const onChangePage = (newValue: number) => {
+    history.push(getUsersPath(newValue, rowsPerPage));
+  };
+  const onChangeRowsPerPage = (newValue: number) => {
+    storeRowsPerPage(newValue);
+    history.push(getUsersPath(page, newValue));
+  }; */
+
   return (
     <div>
       <TableToolbar2
         title="support.roadmap"
         //
-        /* filters={filters}
+        onAddClick={setDialogAddEdit}
+        //
+        filters={filters}
         initFilters={initFilters}
-        onFilterClick={handleRoadmapFilterClick} */
+        onFilterClick={setDialogFilters}
       />
 
-      <Tooltip title="Roadmap">
-        <IconButton aria-label="Add" onClick={() => setDialogAddEdit({})}>
-          <AddComment />
-        </IconButton>
-      </Tooltip>
+      <RoadmapList
+        data={data}
+        onEdit={setDialogAddEdit}
+        onDelete={(payload) =>
+          dispatch(roadmapActions.deleteItem({ payload, meta }))
+        }
+        loading={loading}
+      />
 
-      <Timeline position="alternate" style={{ opacity: loading ? 0.25 : 1 }}>
-        {data &&
-          data.map((item: RoadmapType) => {
-            return (
-              <TimelineItem key={item.icarusRoadmapLogId}>
-                <TimelineOppositeContent></TimelineOppositeContent>
-                <TimelineSeparator>
-                  <TimelineDot className={checkIconColor(item.status)}>
-                    {checkIcon(item.status)}
-                  </TimelineDot>
-                  <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                  <Paper elevation={3} className={classes.paper}>
-                    <Typography variant="h6" component="h1">
-                      {item.title}
-                    </Typography>
-                    <Typography className={classes.marginY}>
-                      {item.description}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      Estimated: {item.estimatedTimeFormatted}
-                    </Typography>
-                    <div className={classes.iconContainer}>
-                      <IconButton
-                        size="small"
-                        onClick={() => setDialogAddEdit(item)}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton
-                        size="small"
-                        className={classes.deleteIcon}
-                        onClick={() => setDialogWarning(item)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </div>
-                  </Paper>
-                </TimelineContent>
-              </TimelineItem>
-            );
-          })}
-      </Timeline>
-
-      {/* <DialogFormFrame
-        onClose={handleFilterDialogClose}
+      <DialogFormFrame
+        onClose={() => setDialogFilters(undefined)}
         title="general.selectFilters"
-        open={dialogFilterOpen}
+        open={dialogFilters}
       >
         <DialogFormRoadmapLogFilter
-          filters={filters}
-          onClearAll={handleClearAllFilters}
-          onClose={handleFilterDialogClose}
-          onSubmit={handleFilterSubmit}
-          onFilterSelectBasicChange={handleFilterSelectBasicChange}
-          onFilterInputChange={handleFilterInputChange}
-          editDisabled={false}
+          initialData={dialogFilters!}
+          onClose={() => setDialogFilters(undefined)}
+          onSubmit={handleSubmitFilters}
         />
-      </DialogFormFrame> */}
+      </DialogFormFrame>
 
       {dialogAddEdit && (
         <DialogFormFrame
@@ -211,15 +112,6 @@ function Roadmap() {
           />
         </DialogFormFrame>
       )}
-
-      <DialogDelete2
-        data={dialogWarning}
-        text="general.deleteWarning"
-        onSubmit={(payload) =>
-          dispatch(roadmapActions.deleteItem({ payload, meta }))
-        }
-        onClose={() => setDialogWarning(undefined)}
-      />
     </div>
   );
 }
