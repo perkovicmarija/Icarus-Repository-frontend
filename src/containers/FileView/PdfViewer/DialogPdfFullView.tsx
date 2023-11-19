13f269ba241bae0f0caf759cb5dbcc9620c6b97a
croraf <vrafaeli@msn.com>
2023-11-19 11:37:04 +0100

    PdfRenderer - adjust styles

--- src/containers/FileView/PdfViewer/DialogPdfFullView.tsx
+++ src/containers/FileView/PdfViewer/DialogPdfFullView.tsx
@@ -4,13 +4,17 @@ import {
   Dialog,
   IconButton,
   Slide,
+  SlideProps,
   Toolbar,
   Typography,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import PdfFrame from "./PdfFrame";
 
-const Transition = React.forwardRef(function Transition(props, ref) {
+const Transition = React.forwardRef<any, SlideProps>(function Transition(
+  props,
+  ref
+) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
@@ -31,6 +35,9 @@ function DialogPdfFullView({
       PaperProps={{
         style: {
           background: "#808080",
+          display: "flex",
+          flexDirection: "column",
+          overflow: "hidden",
         },
       }}
     >
@@ -51,7 +58,10 @@ function DialogPdfFullView({
       </AppBar>
       <div
         style={{
-          margin: "1.5rem auto",
+          width: "100%",
+          display: "flex",
+          flexGrow: 1,
+          overflow: "hidden",
         }}
       >
         <PdfFrame
--- src/containers/FileView/PdfViewer/PdfFrame.tsx
+++ src/containers/FileView/PdfViewer/PdfFrame.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import BookmarkItem from "./BookmarkItem";
-import { Document, Page, pdfjs } from "react-pdf";
+import {
+  Document,
+  DocumentProps,
+  Outline,
+  Page,
+  Thumbnail,
+  pdfjs,
+} from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
-import { makeStyles } from "@mui/styles";
+import { Box, Button, SxProps } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { display } from "@mui/system";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -21,18 +30,39 @@ const FindState = {
   PENDING: 3,
 };
 
-const useStyles = makeStyles({
-  page: { border: "1px solid gray" },
-  document: {
+const sx: Record<string, SxProps> = {
+  pages: {
     display: "flex",
     flexDirection: "column",
     rowGap: "1rem",
+    margin: "1rem auto",
+    height: "fit-content",
   },
-});
+  thumbnails: (visible) => ({
+    display: visible ? "flex" : "none",
+    background: "white",
+    marginRight: "1rem",
+    padding: "0.5rem 0.5rem",
+    flexDirection: "column",
+    rowGap: "0.5rem",
+    overflowY: "auto",
+    overflowX: "hidden",
+    minWidth: "fit-content",
+    minHeight: "fit-content",
+    height: "100%",
+    "& .react-pdf__Thumbnail__page": {
+      border: "1px solid black",
+    },
+  }),
+};
 
-const PdfFrame = ({ file }: { file: any }) => {
-  const classes = useStyles();
-
+const PdfFrame = ({
+  file,
+  showDownload,
+}: {
+  file: any;
+  showDownload?: boolean;
+}) => {
   const [numPages, setNumPages] = useState<number>(1);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
@@ -43,18 +73,67 @@ const PdfFrame = ({ file }: { file: any }) => {
 
   for (let i = 1; i <= numPages; i++) {
     pages.push(
-      <Page key={i} pageNumber={i} scale={1.5} className={classes.page} />
+      <Box
+        sx={
+          {
+            /* border: "1px solid gray" */
+          }
+        }
+      >
+        <Page key={i} pageNumber={i} scale={1.5} />
+      </Box>
     );
   }
 
+  const [showThumbnails, setShowThumbnails] = useState(true);
+
   return (
-    <Document
-      file={file.data}
-      onLoadSuccess={onDocumentLoadSuccess}
-      className={classes.document}
+    <Box
+      sx={{
+        display: "flex",
+        flexGrow: 1,
+        flexDirection: "column",
+        overflow: "hidden",
+        width: "100%",
+        position: "relative",
+        "& .react-pdf__Document": {
+          display: "flex",
+          width: "100%",
+          flexGrow: 1,
+          overflow: "hidden",
+        },
+      }}
     >
-      {pages}
-    </Document>
+      <Box sx={{ background: "white", borderBottom: "1px solid black" }}>
+        <Button
+          variant="text"
+          onClick={() => setShowThumbnails(!showThumbnails)}
+        >
+          Thumbnails
+        </Button>
+        Toolbar TODO
+      </Box>
+      <Document
+        file={file.data}
+        onLoadSuccess={onDocumentLoadSuccess}
+        //className={classes.document}
+      >
+        <Box sx={sx.thumbnails(showThumbnails)}>
+          {Array.from(new Array(numPages), (_el, index) => (
+            <Box
+              sx={{ textAlign: "center", fontSize: "0.85rem" }}
+              key={index + 1}
+            >
+              <Thumbnail pageNumber={index + 1} width={100} />
+              {index + 1}
+            </Box>
+          ))}
+        </Box>
+        <Box sx={{ overflow: "auto", flexGrow: 1, display: "flex" }}>
+          <Box sx={sx.pages}>{pages}</Box>
+        </Box>
+      </Document>
+    </Box>
   );
 };
 
